6a94706b82bbf11a34f5596af9d710831e2d768c
fakhirz <muhammadfakhir111@gmail.com>
2024-09-19 00:43:11 -0700

    Added VITE_BASE_URL for deployment

--- src/hooks/useFecth.js
+++ src/hooks/useFecth.js
@@ -1,30 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 export function useFetch(fetchFn, initialValue) {
-	const [ isFetching, setIsFetching ] = useState(false);
-	const [ error, setError ] = useState();
-	const [ fetchedData, setFetchedData ] = useState(initialValue);
-	useEffect(
-		() => {
-			async function fetchData() {
-				try {
-					setIsFetching(true);
-					const data = await fetchFn();
-					setFetchedData(data);
-				} catch (error) {
-					setError({
-						message: error.mesage || 'Failed to fetch data!'
-					});
-				}
-				setIsFetching(false);
-			}
-			fetchData();
-		},
-		[ fetchFn ]
-	);
-	return {
-		isFetching,
-		error,
-		fetchedData,
-		setFetchedData
-	};
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsFetching(true);
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({
+          message: error.mesage || "Failed to fetch data!",
+        });
+      }
+      setIsFetching(false);
+    }
+    fetchData();
+  }, [fetchFn]);
+  return {
+    isFetching,
+    error,
+    fetchedData,
+    setFetchedData,
+  };
 }
--- src/http.js
+++ src/http.js
@@ -1,31 +1,32 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 export async function fetchAvailablePlaces() {
-	const response = await fetch('http://localhost:3000/places');
-	const resData = await response.json();
-	if (!response.ok) {
-		throw Error('Failed to fetch places!');
-	}
-	return resData.places;
+  const response = await fetch(`${BASE_URL}/places`);
+  const resData = await response.json();
+  if (!response.ok) {
+    throw Error("Failed to fetch places!");
+  }
+  return resData.places;
 }
 export async function fetchUserPlaces() {
-	const response = await fetch('http://localhost:3000/user-places');
-	const resData = await response.json();
-	if (!response.ok) {
-		throw Error('Failed to fetch User places!');
-	}
-	return resData.places;
+  const response = await fetch(`${BASE_URL}/user-places`);
+  const resData = await response.json();
+  if (!response.ok) {
+    throw Error("Failed to fetch User places!");
+  }
+  return resData.places;
 }
 
 export async function updateUserPlaces(places) {
-	const response = await fetch('http://localhost:3000/user-places', {
-		method: 'PUT',
-		body: JSON.stringify({ places }),
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
-	const resData = await response.json();
-	if (!response.ok) {
-		throw Error('Failed to update user data.');
-	}
-	return resData.message;
+  const response = await fetch(`${BASE_URL}/user-places`, {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const resData = await response.json();
+  if (!response.ok) {
+    throw Error("Failed to update user data.");
+  }
+  return resData.message;
 }
